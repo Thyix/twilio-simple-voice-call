c882461325c7be43db0d37454ea0a33f6308ad12
William Garneau <wgarneau@nexapp.ca>
2020-02-28 15:37:17 -0500

    feat/Added react bridge module

--- ios/TwilioSimpleVoiceCall/TwilioSimpleVoiceCall.h
+++ ios/TwilioSimpleVoiceCall/TwilioSimpleVoiceCall.h
@@ -6,7 +6,10 @@
 //  Copyright © 2020 William Garneau. All rights reserved.
 //
 
+#import <React/RTCBridgeModule.h>
+#import <React/RTCEventEmitter.h>
 
-@interface TwilioSimpleVoiceCall
+
+@interface TwilioSimpleVoiceCall : RTCEventEmitter <RTCBridgeModule>
 
 @end
--- ios/TwilioSimpleVoiceCall/TwilioSimpleVoiceCall.m
+++ ios/TwilioSimpleVoiceCall/TwilioSimpleVoiceCall.m
@@ -7,6 +7,7 @@
 //
 
 #import "TwilioSimpleVoiceCall.h"
+#import <React/RCTLog.h>
 
 @import AVFoundation;
 @import PushKit;

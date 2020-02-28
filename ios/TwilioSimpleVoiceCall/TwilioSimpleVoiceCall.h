95e199e9e5aeff3533a77dff39b679e9ff2817a2
William Garneau <wgarneau@nexapp.ca>
2020-02-27 23:23:24 -0500

    feat/Added the old ios file

---
+++ ios/TwilioSimpleVoiceCall/TwilioSimpleVoiceCall.h
@@ -0,0 +1,15 @@
+//
+//  TwilioSimpleVoiceCall.h
+//  TwilioSimpleVoiceCall
+//
+//  Created by William Garneau on 2020-02-27.
+//  Copyright © 2020 William Garneau. All rights reserved.
+//
+
+#import <React/RTCBridgeModule.h>
+#import <React/RTCEventEmitter.h>
+
+
+@interface TwilioSimpleVoiceCall : RTCEventEmitter <RTCBridgeModule>
+
+@end
